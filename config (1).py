3ed7fd1daa44fe88f62ab4574aa77051800b9920
igniteownerpp <ignitevps@outlook.com>
2025-01-25 02:33:21 +0530

    Add files via upload

---
+++ config (1).py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "8005805222:AAGyauPyKZNrAnSWsd5bXO0ygaZqLqCnKMw"
+ADMIN_IDS = ["5579438195"]
+GROUP_ID = -1002470604908  # Replace with your group ID
+GROUP_LINK = "https://t.me/DDOS_SELLER_12"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
