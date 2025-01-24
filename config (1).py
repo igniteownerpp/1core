831be170861c7c1b59edb9e5ef67c4a724f6eb60
igniteownerpp <ignitevps@outlook.com>
2025-01-25 02:35:13 +0530

    Update config.py

--- config (1).py
+++ config (1).py
@@ -1,6 +1,6 @@
-BOT_TOKEN = "8005805222:AAGyauPyKZNrAnSWsd5bXO0ygaZqLqCnKMw"
-ADMIN_IDS = ["5579438195"]
-GROUP_ID = -1002470604908  # Replace with your group ID
-GROUP_LINK = "https://t.me/DDOS_SELLER_12"  # Replace with your group invite link
+BOT_TOKEN = "7538304532:AAFUr8C87Zv44d3Ud-5lQC40UkPxtOFps4g"
+ADMIN_IDS = ["1604629264"]
+GROUP_ID = -1002201895980  # Replace with your group ID
+GROUP_LINK = "https://t.me/+OnxaFJPwFDQyNWI1"  # Replace with your group invite link
 # Default number of threads for attacks
 DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
